9fb972a3666d5291b1c581cca3f659a26e3cb5eb
mhfh611 <mhfh611@outlook.com>
2016-02-22 15:43:33 +0800

    Update is_palindrome.py

--- is_palindrome.py
+++ is_palindrome.py
@@ -1,18 +1,15 @@
 def is_palindrome(n):
     nss = str(n)
-    if(len(nss) == 1):
+    i = 0
+    while(i<len(nss)):
+        if(nss[i] == nss[len(nss) - 1 - i]):
+            i = i + 1
+        else:
+            return False                
+    if(i == len(nss)):
         return True
     else:
-        i = 0
-        while(i<len(nss)):
-            if(nss[i] == nss[len(nss) - 1 - i]):
-                i = i + 1
-            else:
-                return False                
-        if(i == len(nss)):
-            return True
-        else:
-            return False
+        return False
 
 #Test
 output = filter(is_palindrome, range(1,1000))
