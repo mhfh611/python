7e6eadece3a7e61cb5ebd97041e14b2961b80aab
mhfh611 <mhfh611@outlook.com>
2016-02-22 15:41:19 +0800

    Create is_palindrome.py

---
+++ is_palindrome.py
@@ -0,0 +1,19 @@
+def is_palindrome(n):
+    nss = str(n)
+    if(len(nss) == 1):
+        return True
+    else:
+        i = 0
+        while(i<len(nss)):
+            if(nss[i] == nss[len(nss) - 1 - i]):
+                i = i + 1
+            else:
+                return False                
+        if(i == len(nss)):
+            return True
+        else:
+            return False
+
+#Test
+output = filter(is_palindrome, range(1,1000))
+print(list(output))
